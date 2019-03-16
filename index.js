1428d5aa60af1f646f7cf50f3107118910ee99b5
Zune <jjgransbury@hotmail.com>
2019-03-16 15:07:55 +0000

    added default message

--- index.js
+++ index.js
@@ -7,13 +7,10 @@ const x = setInterval(() => {
 
   const distance = endOfDay - now
 
-  const hours = Math.floor(
-    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-  )
   const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
   const seconds = Math.floor((distance % (1000 * 60)) / 1000)
 
-  timer.textContent = `0${hours}:${minutes}:${seconds}`
+  timer.textContent = `${minutes}:${seconds}`
 
   if (distance < 0) {
     clearInterval(x)
